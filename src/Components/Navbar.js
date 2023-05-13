b5affaaa7dd2bb0597b63cd13b281216c094fcc1
Karthika506 <karthimudduluru3@gmail.com>
2023-05-13 21:16:59 +0530

    commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,10 +2,17 @@ import React from "react";
 import image from "../images/react-icon-small.png";
 function Navbar() {
   return (
-    <nav>
-      <img src={image} className="nav--icon" />
+    <nav className={props.darkMode ? "dark" : ""}>
+      <img className="nav--logo_icon" src="./images/react-icon-small.png" />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <h4 className="nav--title">React Course - Project 1</h4>
+
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 }
